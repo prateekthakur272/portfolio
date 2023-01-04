c067fdb1ad2274cdd954cf523614191644107153
Prateek Thakur <67188426+prateekthakur272@users.noreply.github.com>
2023-01-04 18:48:31 +0530

    Started

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:portfolio/pages/home_page.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Portfolio",
+    home: HomePage(),
+  ));
+}
